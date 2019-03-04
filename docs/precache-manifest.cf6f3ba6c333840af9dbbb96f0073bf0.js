27e19392e411abf1fdc2e0e0018a96e3561aa3c3
surmiran <zoid@gmx.ch>
2019-03-04 21:39:44 +0100

    build

---
+++ docs/precache-manifest.cf6f3ba6c333840af9dbbb96f0073bf0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d03e2e3988e268d968b41618c9c468e",
+    "url": "/karl/img/Magnite_icon.9d03e2e3.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/karl/robots.txt"
+  },
+  {
+    "revision": "ab8cc58699d16590721c",
+    "url": "/karl/js/chunk-vendors.e73e1ddb.js"
+  },
+  {
+    "revision": "3b1f471e9f1e82b1b329",
+    "url": "/karl/js/app.448098d5.js"
+  },
+  {
+    "revision": "8760ede29783b50ab5039e9fcc786d77",
+    "url": "/karl/index.html"
+  },
+  {
+    "revision": "85059f61622a143858b71082ebd375a2",
+    "url": "/karl/img/Umanite_icon.85059f61.png"
+  },
+  {
+    "revision": "efaa5489d9aff2d755de1a43c1dcbf69",
+    "url": "/karl/img/Jadiz_icon.efaa5489.png"
+  },
+  {
+    "revision": "d0a8f68b51427e522e2ad712a7a00612",
+    "url": "/karl/img/Enor_pearl_icon.d0a8f68b.png"
+  },
+  {
+    "revision": "6784874231efdf3aceb1dda841b0d5e2",
+    "url": "/karl/img/Croppa_icon.67848742.png"
+  },
+  {
+    "revision": "ae9293f78cc72999f326e084741965a0",
+    "url": "/karl/img/Bismor_icon.ae9293f7.png"
+  },
+  {
+    "revision": "e112f3fc19044b22827020e590dcd60c",
+    "url": "/karl/favicon-16x16.png"
+  },
+  {
+    "revision": "3b1f471e9f1e82b1b329",
+    "url": "/karl/css/app.0f13b548.css"
+  }
+];

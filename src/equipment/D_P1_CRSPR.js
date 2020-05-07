48e6f6487a9b3f044c301dd87fce852f2d55c027
Cheithroid <37781911+Cheithroid@users.noreply.github.com>
2020-05-07 15:44:42 -0400

    forgot a bracket lol

--- src/equipment/D_P1_CRSPR.js
+++ src/equipment/D_P1_CRSPR.js
@@ -24,6 +24,7 @@ export default {
 			dps: damagePerSecond, // damage per second
 			dpa: totalDamage // total damage available
 		};
+	},
 	baseStats: {
 		dmg: { name: "Damage", value: 10 },
 		ex11: { name: "Heat", value: 10 },

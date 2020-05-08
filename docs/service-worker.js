b719047a8d29b901426ce54f86f189f99cc0cdf8
surmiran <zoid@gmx.ch>
2020-05-08 11:49:49 +0200

    Build v1.6.6

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/karl/precache-manifest.481f07d2fe2cbbd9020339734a986d09.js"
+  "/karl/precache-manifest.6bdcf1d4b8ae64e0240c50aa556c89c0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "karl"});

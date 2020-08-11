c5fb958e0f936a129e3613f33715fc8d42469ad8
surmiran <zoid@gmx.ch>
2020-08-11 22:04:41 +0200

    Info modal for karl.gg

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import store from "./store";
 // import "./registerServiceWorker";
 import Toasted from "vue-toasted";
 import Popover  from 'vue-js-popover'
+import VModal from 'vue-js-modal';
 
 Vue.config.productionTip = false;
 Vue.use(Toasted);
+Vue.use(VModal);
 
 Vue.use(Popover);
 
